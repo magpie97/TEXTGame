96ff19c15e735d77b1e3737e793cab51036f3318
Leegyeun <4865213@gmail.com>
2023-03-07 02:31:40 +0900

    game

--- TEXT_RPG/TEXT_RPG.cpp
+++ TEXT_RPG/TEXT_RPG.cpp
@@ -50,6 +50,12 @@ void EnterLobby();
 void PrintMessage(const char* msg);
 void CreatePlayer(StatInfo* playerInfo);
 void PrintStatInfo(const char* name, const StatInfo& info);
+void EnterGame(StatInfo* playerInfo);
+void CreateMonsters(StatInfo monsterInfo[], int count);
+bool EnterBattle(StatInfo* playerInfo, StatInfo* monsterInfo);
+
+
+
 
 
 int main()
@@ -73,7 +79,9 @@ void EnterLobby()
         // 플레이어
         StatInfo playerInfo;
         CreatePlayer(&playerInfo);
-        PrintStatInfo
+        PrintStatInfo("Player", playerInfo);
+
+        EnterGame(&playerInfo);
 
 
     }
@@ -139,5 +147,123 @@ void PrintStatInfo(const char* name, const StatInfo& info)
     cout << name << " : HP = " << info.hp << " ATT = " << info.attack << " DEF = " << info.defence << endl;
     cout << "*********" << endl;
 
+}
+
+void EnterGame(StatInfo* playerInfo)
+{
+    const int MONSTER_COUNT = 2;
+
+    PrintMessage("게임에 입장 하였습니다");
+    
+
+    while (true)
+    {
+        StatInfo monsterInfo[MONSTER_COUNT];
+        CreateMonsters(monsterInfo, MONSTER_COUNT);
+
+        for (int i = 0; i < MONSTER_COUNT; i++)
+        {
+            PrintStatInfo("Monster", monsterInfo[i]);
+
+
+        }
+
+        PrintStatInfo("Player", *playerInfo);
+
+        PrintMessage("1. 전투  2. 전투  3. 도망");
+        int input;
+        cin >> input;
+        if (input == 1 || input == 2)
+        {
+            int index = input - 1;
+            bool victory = EnterBattle(playerInfo, &monsterInfo[index]);
+            if (victory == false)
+                break;
+        }
+
+
+    }
+}
+
+void CreateMonsters(StatInfo monsterInfo[], int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        int randValue = rand() % 3;
+
+
+        switch (randValue)
+        {
+        case MT_Slime:
+            monsterInfo->hp = 30;
+            monsterInfo->attack = 2;
+            monsterInfo->defence = 0;
+        
+            break;
+        
+        case MT_Skeleton:
+            monsterInfo->hp = 15;
+            monsterInfo->attack = 8;
+            monsterInfo->defence = 1;
+            
+            break;
+
+        case MT_Orc:
+            monsterInfo->hp = 50;
+            monsterInfo->attack = 10;
+            monsterInfo->defence = 4;
+            
+            break;
+
+        default:
+            break;
+        }
+
+    }
+
+}
+
+bool EnterBattle(StatInfo* playerInfo, StatInfo* monsterInfo)
+{
+    while (true)
+    {
+        int damage = playerInfo->attack - monsterInfo->defence;
+        if (damage < 0)
+            damage = 0;
+
+        monsterInfo->hp -= damage;
+        if (monsterInfo->hp < 0)
+            monsterInfo->hp = 0;
+
+        PrintStatInfo("Monster", *monsterInfo);
+
+        if (monsterInfo->hp == 0)
+        {
+            PrintMessage("몬스터를 처치했습니다");
+            return true;
+        }
+
+        damage = monsterInfo->attack - playerInfo->defence;
+        if (damage < 0)
+            damage = 0;
+
+        playerInfo->hp -= damage;
+        if (playerInfo->hp < 0)
+            playerInfo->hp = 0;
+
+
+        PrintStatInfo("Player", *playerInfo);
+
+        if (playerInfo->hp == 0)
+        {
+            PrintMessage("Game Over");
+
+            return false;
+        }
+
+    }
+    
+
+    return false;
 }
 
